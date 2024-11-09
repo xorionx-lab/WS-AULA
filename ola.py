2aaad6119a694be9d8ee19be625e551cfc0739d3
xorionx-lab <dennis.grupoaffectio@gmail.com>
2024-11-09 16:58:35 -0300

    atual

--- ola.py
+++ ola.py
@@ -1,2 +1,7 @@
 print('tudo bem')
-nome = str(input("Digitre seu nome aqui:"))
+a= input("coloque seu nome")
+nome = str(input("Digite seu nome aqui:"))
+if a == nome:
+    print("Cadastro realizado com sucesso")
+else:
+    print("cadastro incorreto")
